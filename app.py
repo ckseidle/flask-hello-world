68728ab2abff95eadf179cacc5c04a7fd6565fde
ckseidle <chse6836@colorado.edu>
2024-04-01 16:20:45 -0400

    db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
 import psycopg2
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,3 +12,20 @@ def testing():
     conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
     conn.close()
     return 'Connected to the database!'
+
+app.route('/db_create')
+def creating():
+    conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table created!'
