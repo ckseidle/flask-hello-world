35ba3ace569bdcb013d92e545e58cf0c87a3f13f
ckseidle <chse6836@colorado.edu>
2024-04-01 16:35:33 -0400

    db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import psycopg2
+
 from flask import Flask
+from PyPI import psycopg2-binary
 
 app = Flask(__name__)
 
@@ -7,13 +8,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from Christian Seidle in 3308!'
 
-app.route('/db_test')
+@app.route('/db_test')
 def testing():
     conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
     conn.close()
     return 'Connected to the database!'
 
-app.route('/db_create')
+@app.route('/db_create')
 def creating():
     conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
     cur = conn.cursor()
@@ -29,3 +30,38 @@ def creating():
     conn.commit()
     conn.close()
     return 'Basketball table created!'
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number Values)
+        VALUES ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'Golden State', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table populated!'
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect('postgres://lab10_sql_user:w9IrSpCQMvqvMCZMF5HVckOnhSbGC4mv@dpg-co5h25uv3ddc73911mh0-a/lab10_sql')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Basketball')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ''
+    response_string+='<table>'
+    for player in records:
+        response_string+='<tr>'
+        for info in player:
+            response_string+='<td>{}</td>'.format(info)
+        response_string+='</tr>'
+    response_string+='</table>'
+    return response_string
+
+app.route('/db_drop')
+
